5bdfb4036fdfd2edb9399f490a21ca68ce7c2831
Martynas <m.kulys@student.tue.nl>
2024-04-25 12:02:05 +0200

    Minor changes

--- Split 1/main.py
+++ Split 1/main.py
@@ -1,47 +1,66 @@
 import json
 import os
+from typing import List
 
-directory = r"C:\Users\User\Desktop\BDS\DBL Data Challenge\data\\"
+data_directory: str = r"/data/"
+current_directory: str = os.getcwd()
 
 
-def list_files():
-    files_list = []
-    files = os.listdir(directory)
-    print("Files in the directory:")
+def lufthansa_related(tweet: str) -> bool:
+    """
+    Checks if the tweet is related to Lufthansa.
+    :param tweet: the string item of the tweet.
+    :return: True if related to Lufthansa, False otherwise.
+    """
+    if "124476322" in tweet or "lufthansa" in tweet or "Lufthansa" in tweet:
+        return True
+    return False
+
+
+def list_files() -> List[str]:
+    """
+    Gets all files from a directory.
+    :return: the list of files from the directory.
+    """
+    files_list: List[str] = []
+    files: List[str] = os.listdir(current_directory+data_directory)
     for file in files:
-        print(file)
         files_list.append(file)
     return files_list
 
 
-def read_file(file_name):
-    data_to_append = []
-    print(f">>> {file_name}")
-    # Open the JSON file for reading
-    with open(directory+file_name, 'r') as file:
-        # Read the entire file content
-        file_content = file.read()
-
-        # Split the content into separate JSON objects
-        json_objects = file_content.strip().split('\n')
+def read_file(file_name: str) -> List[str]:
+    """
+    Reads data (tweets) from the json file.
+    :param file_name: the file to read from.
+    :return: data to append to the output file.
+    """
+    data_to_append: List[str] = []
+    with open(current_directory+data_directory+file_name, 'r') as file:
+        file_content: str = file.read()
+        json_objects: List[str] = file_content.strip().split('\n')
         for item in json_objects:
             try:
-                if "124476322" in item or "lufthansa" in item or "Lufthansa" in item:
-                    temp = json.loads(item)
-                    # print(temp)
-                    data_to_append.append(temp)
+                if lufthansa_related(item):
+                    luft_tweet = json.loads(item)
+                    data_to_append.append(luft_tweet)
             except Exception as e:
                 continue
     return data_to_append
 
 
-def write_to_file(data):
-    with open(r"C:\Users\User\Desktop\BDS\DBL Data Challenge\airlines\lufthansa.json", 'a', encoding='utf-8') as file:
+def write_to_file(data: List[str]) -> None:
+    """
+    Writes data to the output file.
+    :param data: information to write to the file.
+    :return: nothing.
+    """
+    with open(fr"{current_directory}/airlines/lufthansa.json", 'a', encoding='utf-8') as file:
         for string in data:
             file.write(str(string) + '\n')
 
 
-x = list_files()
-for file_thing in x:
-    y = read_file(file_thing)
-    write_to_file(y)
+files_in_root = list_files()
+for json_file in files_in_root:
+    tweet_info = read_file(json_file)
+    write_to_file(tweet_info)
