295d758e6a6c2106a9ce9990f4cbb8ad03dfbfbc
20232075 <n.seleznov@student.tue.nl>
2024-05-02 20:50:56 +0200

    .

--- Split 1/database_creation.py
+++ Split 1/database_creation.py
@@ -14,64 +14,56 @@ def create_db(user: str, database: str, password: str, host: str):
     except Error as e:
         return e
 
-    users = """
-        CREATE TABLE IF NOT EXISTS users(
-            user_id BIGINT PRIMARY KEY,
-            location VARCHAR(50),
+    users = """ CREATE TABLE IF NOT EXISTS Users(
+            user_id VARCHAR(255) PRIMARY KEY,
+            country_code VARCHAR(255),
             verified TINYINT NOT NULL,
             followers_count BIGINT NOT NULL,
             friends_count BIGINT NOT NULL,
             listed_count BIGINT NOT NULL,
-            favourites_count BIGINT NOT NULL,
             statuses_count BIGINT NOT NULL,
+            favourites_count BIGINT NOT NULL,
             creation_time TIMESTAMP NOT NULL,
-            protected BOOL NOT NULL
-        );
-        """
+            defaul_profile TINYINT NOT NULL,
+            default_profile_image TINYINT NOT NULL            
+        );"""
 
-    tweets = """
-        CREATE TABLE IF NOT EXISTS tweets(
-            tweet_id BIGINT PRIMARY KEY,
-            user_id BIGINT NOT NULL,
-            text VARCHAR(65535) NOT NULL,
-            lang VARCHAR(30) NOT NULL,
+    tweets = """ CREATE TABLE IF NOT EXISTS Tweets(
+            tweet_id VARCHAR(255) PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            full_text VARCHAR(65535) NOT NULL,
+            lang VARCHAR(255) NOT NULL,
             creation_time TIMESTAMP NOT NULL,
-            place_full_name VARCHAR(50) NOT NULL,
+            place_full_name VARCHAR(255) NOT NULL,
             favorite_count INT NOT NULL,
-            retweet_count INT NOT NULL
-        );
-        """
+            retweet_count INT NOT NULL,
+            source_name VARCHAR(255) NOT NULL,
+            country_code VARCHAR(10),
+            possibly_sensitive TINYINT NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES Users(user_id)
+        );"""
 
     replies = """
-        CREATE TABLE IF NOT EXISTS replies(
-            tweet_id BIGINT PRIMARY KEY,
-            replied_tweet_id BIGINT NOT NULL,
-            reply_count BIGINT NOT NULL
-        );
-        """
+        CREATE TABLE IF NOT EXISTS Replies(
+            tweet_id VARCHAR(255) PRIMARY KEY,
+            replied_tweet_id VARCHAR(255) NOT NULL,
+            reply_count BIGINT NOT NULL,
+            FOREIGN KEY (tweet_id) REFERENCES Tweets(tweet_id) ON DELETE CASCADE
+        );"""
 
     quotes = """
-        CREATE TABLE IF NOT EXISTS quotes(
-            tweet_id INT PRIMARY KEY,
-            quoted_status_id BIGINT NOT NULL,
-            quote_count INT NOT NULL
-        );
-        """
-
-    hashtags = """
-        CREATE TABLE IF NOT EXISTS hashtags(
-            hashtag_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-            tweet_id BIGINT NOT NULL,
-            hashtag VARCHAR(60) NOT NULL
-        );
-        """
+        CREATE TABLE IF NOT EXISTS Quotes(
+            tweet_id VARCHAR(255) PRIMARY KEY,
+            quoted_status_id VARCHAR(255) NOT NULL,
+            quote_count BIGINT NOT NULL,
+            FOREIGN KEY (tweet_id) REFERENCES Tweets(tweet_id) ON DELETE CASCADE
+        );"""
 
     cursor = connection.cursor()
     cursor.execute(users)
     cursor.execute(tweets)
     cursor.execute(replies)
     cursor.execute(quotes)
-    cursor.execute(hashtags)
     connection.close()
 
 
