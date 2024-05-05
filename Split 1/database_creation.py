167422b29af15ed81f746448d65288dc7e5cfa3f
Kirill Chekmenev <chekmenev2004@mail.ru>
2024-05-05 10:40:42 +0200

    added check for duplicates and odd objects + semi optimal script for inserting data into database

--- Split 1/database_creation.py
+++ Split 1/database_creation.py
@@ -1,4 +1,5 @@
 from os import getenv
+import os
 import mysql.connector
 from mysql.connector import Error, InterfaceError, OperationalError
 import json
@@ -7,7 +8,24 @@ from tqdm import tqdm
 import time
 
 
-def connect_to_database(user_fill, database_fill, password_fill, host_fill):
+# TODO: count connect disconnect
+def connect_to_database(
+    user_fill: str, database_fill: str, password_fill: str, host_fill: str
+) -> mysql.connector.connection.MySQLConnection:
+    """
+    Connect to a database using the provided credentials.
+
+    Args:
+        user_fill (str): The username for the database connection.
+        database_fill (str): The name of the database to connect to.
+        password_fill (str): The password for the database user.
+        host_fill (str): The host address of the database.
+
+    Returns:
+        mysql.connector.connection.MySQLConnection: A connection to the database.
+
+    """
+
     return mysql.connector.connect(
         user=user_fill,
         database=database_fill,
@@ -30,13 +48,11 @@ def create_db(user: str, database: str, password: str, host: str) -> None:
     Returns:
         None if successful, Error object if connection or table creation fails.
     """
-    try:
-        # Establish a connection to MySQL
-        connection = connect_to_database(user, database, password, host)
-    except Error as e:
-        return e
+    connection: mysql.connector.connection.MySQLConnection = connect_to_database(
+        user, database, password, host
+    )
 
-    users = """ CREATE TABLE IF NOT EXISTS Users(
+    users: str = """ CREATE TABLE IF NOT EXISTS Users(
             user_id VARCHAR(255) PRIMARY KEY,
             location VARCHAR(255),
             verified TINYINT NOT NULL,
@@ -48,7 +64,7 @@ def create_db(user: str, database: str, password: str, host: str) -> None:
             default_profile_image TINYINT NOT NULL            
         );"""
 
-    tweets = """ CREATE TABLE IF NOT EXISTS Tweets(
+    tweets: str = """ CREATE TABLE IF NOT EXISTS Tweets(
             tweet_id VARCHAR(255) PRIMARY KEY,
             user_id VARCHAR(255) NOT NULL,
             full_text VARCHAR(255) NOT NULL,
@@ -73,6 +89,17 @@ def create_db(user: str, database: str, password: str, host: str) -> None:
 
 
 def insert_batch_data(cursor, batch_data):
+    """
+    Inserts batch data into Users and Tweets tables.
+
+    Args:
+        cursor: Database cursor to execute SQL queries.
+        batch_data: List of tuples containing user and tweet data to be inserted.
+
+    Returns:
+        None
+    """
+
     if not batch_data:
         return
 
@@ -87,31 +114,42 @@ def insert_batch_data(cursor, batch_data):
     retweet_count, source_link, possibly_sensitive, replied_tweet_id, reply_count, quoted_status_id, quote_count)
     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
     """
-
+    user_data = []
+    tweet_data = []
     for data in batch_data:
-        try:
-            cursor.execute(insertion_user, data[1])
-            cursor.execute(insertion_tweets, data[3])
-        except (InterfaceError, OperationalError) as ex:
-            print(f"Error inserting data: {ex}")
-            cursor._cnx.rollback()
-            raise
-        except Exception as ex:
-            print(f"Error inserting data: {ex}")
-            cursor._cnx.rollback()
-        else:
-            cursor._cnx.commit()
+        user_data.append(data[0])
+        tweet_data.append(data[1])
+
+    cursor.executemany(insertion_user, user_data)
+    cursor.executemany(insertion_tweets, tweet_data)
+    cursor._cnx.commit()
 
 
 def process_json_object(dict_):
-    if 'delete' in dict_:
+    """
+    Processes a JSON object to extract user and tweet data.
+
+    Args:
+        dict_: Dictionary representing a JSON object containing user and tweet information.
+
+    Returns:
+        Tuple containing user data and tweet data extracted from the JSON object.
+    """
+
+    if "delete" in dict_:
         return None
 
     user = dict_.get("user", {})
 
-    text = dict_.get("full_text", dict_.get("text", ""))
+    text = dict_.get(
+        "full_text", dict_.get("text", "")
+    )  # TODO: what do we do with empty text???
     date_string = user.get("created_at", "")
-    date_object = datetime.strptime(date_string, "%a %b %d %H:%M:%S %z %Y") if date_string else None
+    date_object = (
+        datetime.strptime(date_string, "%a %b %d %H:%M:%S %z %Y")
+        if date_string
+        else None
+    )
     timestamp = date_object.timestamp() if date_object else None
 
     user_id = user.get("id_str", "")
@@ -120,132 +158,116 @@ def process_json_object(dict_):
     followers_count = user.get("followers_count", 0)
     friends_count = user.get("friends_count", 0)
     statuses_count = user.get("statuses_count", 0)
-    default_profile = int(user.get("default_profile", False))
-    default_profile_image = int(user.get("default_profile_image", False))
+    default_profile = int(user.get("default_profile", True))
+    default_profile_image = int(user.get("default_profile_image", True))
 
-    user_data = (user_id, location, verified, followers_count, friends_count,
-                 statuses_count, timestamp, default_profile, default_profile_image)
+    user_data = (
+        user_id,
+        location,
+        verified,
+        followers_count,
+        friends_count,
+        statuses_count,
+        timestamp,
+        default_profile,
+        default_profile_image,
+    )
 
-    tweet_id = str(dict_['id_str'])
-    lang = str(dict_.get('lang', 'en'))
-    creation_time = int(dict_.get('timestamp_ms', '0')) // 1000
-    country_code = None if dict_.get('place') is None else dict_["place"]["country_code"]
-    favorite_count = int(dict_.get('favorite_count', 0))
-    retweet_count = int(dict_.get('retweet_count', 0))
-    source_link = str(dict_.get('source', ''))
-    possibly_sensitive = int(dict_.get("possibly_sensitive", False))
-    replied_tweet_id = str(dict_.get('in_reply_to_status_id_str'))
-    replied_count = int(dict_.get('replied_count', 0))
-    quoted_tweet_id = str(dict_.get('quoted_tweet_id_str'))
-    quoted_count = int(dict_.get('quoted_count', 0))
+    tweet_id = dict_["id_str"]
+    lang = dict_.get("lang", "en")
+    creation_time = int(dict_.get("timestamp_ms", 0)) // 1000
+    country_code = (
+        None if dict_.get("place") is None else dict_["place"]["country_code"]
+    )
+    favorite_count = dict_.get("favorite_count", 0)
+    retweet_count = dict_.get("retweet_count", 0)
+    source_link = dict_.get("source", "")
+    possibly_sensitive = dict_.get("possibly_sensitive", False)
+    replied_tweet_id = dict_.get("in_reply_to_status_id_str")
+    replied_count = dict_.get("replied_count", 0)
+    quoted_tweet_id = dict_.get("quoted_tweet_id_str")
+    quoted_count = dict_.get("quoted_count", 0)
 
-    tweet_data = (tweet_id, user_id, text, lang, creation_time, country_code,
-                  favorite_count, retweet_count, source_link, possibly_sensitive,
-                  replied_tweet_id, replied_count,quoted_tweet_id, quoted_count)
+    tweet_data = (
+        tweet_id,
+        user_id,
+        text,
+        lang,
+        creation_time,
+        country_code,
+        favorite_count,
+        retweet_count,
+        source_link,
+        possibly_sensitive,
+        replied_tweet_id,
+        replied_count,
+        quoted_tweet_id,
+        quoted_count,
+    )
 
-    return ("Users", user_data, "Tweets", tweet_data)
-
-
-def database_fill(user_fill: str, database_fill: str, password_fill: str, host_fill: str):
-    batch_size = 500
-    retries = 5
-    delay = 1
-    try:
-        with open(r"C:\Users\20232075\PycharmProjects\DBL-Micro-Soft\Split 1\data\cleaned_tweets_combined.json",
-                  'r') as file:
-            file_content = file.read()
-            json_objects = file_content.strip().split(",\n")
-
-            total_iterations = len(json_objects)
-            progress_bar = tqdm(total=total_iterations, desc="Processing")
-            connection = connect_to_database(user_fill, database_fill, password_fill, host_fill)
-            cursor = connection.cursor()
-            batch_data = []
-            for string in json_objects:
-                try:
-                    dict_ = json.loads(string)
-                    data = process_json_object(dict_)
-                    if data:
-                        batch_data.append(data)
-                    if len(batch_data) == batch_size:
-                        insert_batch_with_retries(cursor, batch_data, retries, delay)
-                        batch_data = []
-                    progress_bar.update(1)
-                except Error as ex:
-                    print(f"Error processing record: {string}\nException: {ex}")
-                    continue
-
-            if batch_data:
-                insert_batch_with_retries(cursor, batch_data, retries, delay)
-
-            progress_bar.close()
-
-    except (InterfaceError, OperationalError) as ex:
-        print(f"Connection error: {ex}")
-        if connection:
-            connection.rollback()
-    except Error as e:
-        print(f"Error: {e}")
-        return
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
+    return (user_data, tweet_data)
 
 
-def insert_batch_with_retries(cursor, batch_data, retries, delay):
-    for attempt in range(retries):
-        try:
+def database_fill(
+    user_fill: str, database_fill: str, password_fill: str, host_fill: str
+):
+    batch_size = 1000
+    with open(
+        os.path.join(os.getcwd(), "data", "cleaned_tweets_combined.json"),
+        "r",
+    ) as file:
+        connection = connect_to_database(
+            user_fill, database_fill, password_fill, host_fill
+        )
+        cursor = connection.cursor()
+        batch_data = []
+        for line in tqdm(file, desc="Processing", mininterval=5):
+            tweet_dict = json.loads(line[:-2])
+            if data := process_json_object(tweet_dict):
+                batch_data.append(data)
+            if len(batch_data) == batch_size:
+                insert_batch_data(cursor, batch_data)
+                batch_data = []
+        if batch_data:
             insert_batch_data(cursor, batch_data)
-            break
-        except OperationalError as ex:
-            print(f"Retry {attempt + 1}/{retries} failed: {ex}")
-            time.sleep(delay)
-            delay *= 2
-        except InterfaceError as ex:
-            print(f"Retry {attempt + 1}/{retries} failed: {ex}")
-            cursor.connection = connect_to_database(
-                cursor.connection.user, cursor.connection.database,
-                cursor.connection.password, cursor.connection.server_host
-            )
-            delay *= 2
-        except Exception as ex:
-            print(f"Error inserting data: {ex}")
-            break
+    cursor.close()
+    connection.close()
+
+
+def check_given_var(env_var_str: str) -> str:
+    """
+    Check if the given environment variable is set and return its value.
+
+    Args:
+        env_var_str (str): The name of the environment variable to check.
+
+    Returns:
+        str: The value of the environment variable.
+
+    Raises:
+        AssertionError: If the environment variable is not found.
+    """
+
+    env_var = getenv(env_var_str)
+    assert (
+        env_var is not None
+    ), f"{env_var_str} is required but not found in environment variables"
+    return env_var
+
+
+def check_env_vars() -> (str, str, str, str):  # type: ignore
+    user = check_given_var("DBL_USER")
+    database = check_given_var("DBL_DATABASE")
+    password = check_given_var("DBL_PASSWORD")
+    host = check_given_var("DBL_HOST")
+    return user, database, password, host
+
 
 if __name__ == "__main__":
-    while True:
-        try:
-            user = getenv("DBL_USER")
-            assert (
-                    user is not None
-            ), "DBL_USER is required but not found in environment variables"
-
-            database = getenv("DBL_DATABASE")
-            assert (
-                    database is not None
-            ), "DBL_DATABASE is required but not found in environment variables"
-
-            password = getenv("DBL_PASSWORD")
-            assert (
-                    password is not None
-            ), "DBL_PASSWORD is required but not found in environment variables"
-
-            host = getenv("DBL_HOST")
-            assert (
-                    host is not None
-            ), "DBL_HOST is required but not found in environment variables"
-
-            print("Start database creation on the server")
-            create_db(user, database, password, host)
-            print("Database has been created")
-
-            print("Start database data insertion")
-            database_fill("nezox2um_dbl", "nezox2um_dbl", "OX8tAkhwowXp", "nezox2um.beget.tech")
-            print("Data insertion finished")
-
-            break
-        except Error as e:
-            print(f"Retrying due to error: {e}")
-            time.sleep(5)
+    user, database, password, host = check_env_vars()
+    print("Start database creation on the server")
+    create_db(user, database, password, host)
+    print("Database has been created")
+    print("Start database data insertion")
+    database_fill(user, database, password, host)
+    print("Data insertion finished")
