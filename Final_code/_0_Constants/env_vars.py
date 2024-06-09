80216f242394abc5cd0443636dd2cbcabd489fd9
Igor_Banasik <i.b.banasik@student.tue.nl>
2024-06-09 19:47:53 +0200

    Put the code used to get environment variables in one place

---
+++ Final_code/_0_Constants/env_vars.py
@@ -0,0 +1,27 @@
+
+def check_given_var(env_var_str: str) -> str:
+    """
+    Check if the given environment variable is set and return its value.
+
+    Args:
+        env_var_str (str): The name of the environment variable to check.
+
+    Returns:
+        str: The value of the environment variable.
+
+    Raises:
+        AssertionError: If the environment variable is not found.
+    """
+
+    env_var = check_env_vars(env_var_str)
+    assert (
+        env_var is not None
+    ), f"{env_var_str} is required but not found in environment variables"
+    return env_var
+
+def check_env_vars() -> (str, str, str, str):  # type: ignore
+    user = check_given_var("DBL_USER")
+    database = check_given_var("DBL_DATABASE")
+    password = check_given_var("DBL_PASSWORD")
+    host = check_given_var("DBL_HOST")
+    return user, database, password, host
