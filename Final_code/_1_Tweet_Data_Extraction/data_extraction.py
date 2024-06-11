05abe19d6f0f81314e0d8ff2bf70f70c3972d301
Kirill Chekmenev <chekmenev2004@mail.ru>
2024-06-11 10:54:30 +0200

    adjustments in the code structure to work from the terminal from Final_Code folder and improvements in README.md

--- Final_code/_1_Tweet_Data_Extraction/data_extraction.py
+++ Final_code/_1_Tweet_Data_Extraction/data_extraction.py
@@ -1,23 +1,29 @@
 import json
 import os
+import sys
 from typing import Any, Dict, List
 
 from data_extraction_helpers import delete_existing_file, read_from_file, start_cleaning
 from tqdm.auto import tqdm
 
-current_directory: str = os.getcwd()
-all_tweet_id: set = set()
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "_0_Constants_and_Utils"))
 
 
-def append_to_file(tweets_list: List[Dict[str, Any]]) -> None:
+from defined_paths import folder_path_processed, path_processed_tweets_json
+
+
+def append_to_file(tweets_list: List[Dict[str, Any]], output_file_path: str) -> None:
     """
-    Appends a list of tweets in JSON format to a file.
-    :param tweets_list: List of dictionaries representing tweets to append.
-    :return: None.
+    Appends a list of tweet dictionaries to a file in JSON format.
+
+    Args:
+        tweets_list (List[Dict[str, Any]]): The list of tweet dictionaries to append to the file.
+        output_file_path (str): The path to the output file.
+
+    Returns:
+        None
     """
-    output_file_path: str = os.path.join(
-        "../data_processed", "cleaned_tweets_combined.json"
-    )
+
     with open(output_file_path, "a", encoding="utf-8") as file:
         for tweet in tweets_list:
             if not valid_tweet(tweet):
@@ -58,14 +64,14 @@ def start_general_extraction() -> None:
     3. Reads tweets from each file, processes them, and cleans them.
     4. Appends the cleaned tweets to the output file.
 
-    The folder with the json files to be cleaned must be in the same directory as this file, under the name 'data_raw'.
+    The folder with the json files to be cleaned must be in the same
+    directory as this file, under the name 'data_raw'.
 
-    The cleaned data will be in the directory of this project, under /data_processed/cleaned_tweets_combined.json.
+    The cleaned data will be in the directory of this project, under
+    /data_processed/cleaned_tweets_combined.json.
 
     """
-    output_file_path: str = os.path.join(
-        "../data_processed", "cleaned_tweets_combined.json"
-    )
+    output_file_path: str = path_processed_tweets_json
 
     # Create the output directory if it does not exist
     os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
@@ -74,7 +80,7 @@ def start_general_extraction() -> None:
     delete_existing_file(output_file_path)
 
     # Extract all tweets from files, append them to the output file
-    path_to_all_json_files: str = os.path.join(current_directory, "1_Tweet_Data_Extraction/data_raw")
+    path_to_all_json_files: str = os.path.join(folder_path_processed, "data_raw")
 
     all_raw_json_files: List[str] = os.listdir(path_to_all_json_files)
     for file in tqdm(
@@ -97,8 +103,9 @@ def start_general_extraction() -> None:
                 )
             else:
                 cleaned_tweets_list.append(start_cleaning(tweet))
-        append_to_file(cleaned_tweets_list)  # noqa
+        append_to_file(cleaned_tweets_list, output_file_path)
 
 
 if __name__ == "__main__":
+    all_tweet_id: set = set()
     start_general_extraction()
--- utils/utils.py
+++
@@ -1,2 +0,0 @@
-# TODO: get_data - given the path or mysql parameters and query it gets data
-# TODO: upload_data - given the path or mysql parameters, query and data batch it uploads the data
